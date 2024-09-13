6661bf633b0a2713138a3ba537156d7e1ccb39ea
rimasultana <rimasultana20225@gmail.com>
2024-09-13 16:21:08 +0600

    problem- 4

--- convert.js
+++ convert.js
@@ -18,9 +18,9 @@ return result
 console.log(inchToFeet(75));
  */
 
-
-
-function publicBusFare(People){
+/*
+!publicBusFare
+ function publicBusFare(People){
     const bus = 50;
     const micro = 11;
     const publicBusCost = 250;
@@ -30,4 +30,32 @@ function publicBusFare(People){
     return remainAfterMicro * publicBusCost;
 
 }
-console.log(publicBusFare(117));
+todoconsole.log(publicBusFare(365)); */
+/* 
+function IsJavaScriptFile(str) {
+  const check = str.indexOf(".js");
+  if (check !== -1) {
+    return true;
+  }
+  return false;
+}
+
+console.log(IsJavaScriptFile('app.png'));
+ */
+
+
+function OilPrize(dejal, patrol,octral){
+   const dejalPrize = 114;
+   const patrolPrize = 130;
+   const octralPrize = 135;
+
+
+   const remainDejal = dejal * dejalPrize;
+   const remainPatrol = patrol * patrolPrize;
+   const remainOctral = octral * octralPrize;
+
+   const totalCost = remainDejal + remainOctral + remainPatrol;
+   return totalCost;
+
+}
+console.log(OilPrize(30, 20, 10));
