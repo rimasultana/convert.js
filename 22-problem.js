3430dc0589dc0c6f6f47aa09a5847f27b4dfe112
rimasultana <rimasultana20225@gmail.com>
2024-09-13 22:52:38 +0600

    problem solve-2

--- 22-problem.js
+++ 22-problem.js
@@ -1,6 +1,32 @@
-function celsiusToFahrenheit(celsius){
-const fahrenheit = celsius  * 9 / 5 + 32;
-return fahrenheit;
+function celsiusToFahrenheit(celsius) {
+  const fahrenheit = (celsius * 9) / 5 + 32;
+  return fahrenheit;
 }
-const result = celsiusToFahrenheit(2);
-console.log(result);
+// nodublicate number nar korar function
+
+const numbers = [5, 6, 11, 12, 98, 5];
+
+function doblicateNumbers(array) {
+  const unique = [];
+  for (const item of array) {
+    if (unique.includes(item) === false) {
+      unique.push(item);
+    }
+  }
+  return unique;
+}
+
+let number = [5, 6, 11, 12, 98, 5];
+let target1 = 5;
+ 
+function coundDoblicateNumber(arr, target){
+ let count = 0;
+
+ for(let number of arr){
+  if(number === target){
+    count++;
+  }
+ }
+ return count;
+}
+console.log(coundDoblicateNumber(number,target1));
